b261d3e2a751939a190ea3dad5a2dbc0f4e2f6df
Caroline Rocha <caroline10francarocha@gmail.com>
2026-08-26 11:45:04 -0300

    fix: importar rememberSaveable no formulário

--- app/src/main/java/br/com/carol/todolist/ui/FormularioTarefaScreen.kt
+++ app/src/main/java/br/com/carol/todolist/ui/FormularioTarefaScreen.kt
@@ -3,6 +3,7 @@ package br.com.carol.todolist.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
