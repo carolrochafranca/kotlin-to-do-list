960184e2b05cf9c864fa472dc9cdbc06ff4b431a
Caroline Rocha <caroline10francarocha@gmail.com>
2026-08-26 10:50:24 -0300

    test: adicionar contrato inicial do TarefaDao

---
+++ app/src/androidTest/java/br/com/carol/todolist/TarefaDaoContratoTest.kt
@@ -0,0 +1,11 @@
+package br.com.carol.todolist
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+class TarefaDaoContratoTest {
+    @Test fun `DAO deve manter tarefas observáveis`() {
+        // O teste de integração será executado com banco Room em memória no dispositivo.
+        assertTrue(true)
+    }
+}
